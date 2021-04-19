57be1e65b12fd39a2fbad55f6af66f7d8baaf5ac
LI, Qi <goodlq11@gmail.com>
2021-04-19 18:16:32 -0400

    install script

--- startpod.sh
+++ startpod.sh
@@ -2,16 +2,19 @@
 
 # sudo git clean --force -dX
 
-sudo podman run -it --rm --network=host --volume=/opt/ble2mqtt:/opt/ble2mqtt python:3.8 bash
-# sudo podman run -it --rm --network=host --volume=/opt/ble2mqtt:/opt/ble2mqtt homeassistant/home-assistant:latest bash
-# apk add build-base
-cd /opt/ble2mqtt
-python3 -m venv --copies /opt/ble2mqtt
-source bin/activate
-cp -r /usr/local/lib/python3.8/distutils /opt/ble2mqtt/lib/python3.8/site-packages
-pip install --upgrade bleak pyyaml hbmqtt furl pyee pip
-pip install --upgrade autopep8
+if command -v python3; then
+    python_version=$(python3 --version 2>&1)
+elif command -v python; then
+    python_version=$(python --version 2>&1)
+else
+    : ${python_version?neither python 2 or 3 detected}
+fi
+python_version=$(grep --only-matching '[2-3]\.[0-9][0-9]*\.[0-9][0-9]*' <<< $python_version)
 
-cd /opt/ble2mqtt
-# python3 -m venv --copies /opt/ble2mqtt
-export PYTHONPATH=/opt/ble2mqtt/lib/python3.8/site-packages
+miniconda_tmp=$(mktemp -p /tmp -d)
+curl --insecure --location https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --output "$miniconda_tmp"/conda_scaffold.sh
+bash "$miniconda_tmp"/conda_scaffold.sh -b -p "$miniconda_tmp"/conda_dir
+
+ble2mqtt_opt_dir="${ble2mqtt_opt_dir:-/opt/ble2mqtt}"
+"$miniconda_tmp"/conda_dir/bin/conda create --yes --name ble2mqtt python="$python_version"
+"$miniconda_tmp"/conda_dir/envs/ble2mqtt/bin/pip install --upgrade bleak pyyaml hbmqtt furl pyee pip --target "$ble2mqtt_opt_dir" --no-cache-dir
